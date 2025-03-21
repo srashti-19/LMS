10d2c14182edaa1732c563286a329c4fe88f1f3d
Srashti Rai <srashti909@gmail.com>
2025-03-21 08:06:11 +0530

    change webhooks

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -3,46 +3,104 @@ import User from "../models/User.js"
 
 //API Controller Function to Manage Clerk User with database
 
-export const clerkWebhooks = async(req,res)=>{
-        try{
-           const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
-           await whook.verify(JSON.stringify(req.body),{
-            "svix-id" : req.headers[ "svix-id"],
-            "svix-timestamp" : req.headers[ "svix-timestamp"],
-            "svix-signature" : req.headers[ "svix-signature"]
-           })
-           const {data,type} = req.body
-           switch(type){
-                case 'user.created' : {
-                    const userData = {
-                        _id : data.id,
-                        email : data.email_addresses[0].email_address,
-                        name : data.first_name + " "+ data.last_name,
-                        imageUrl : data.image_url,
-                    }
-                    await User.create(userData)
-                    res.json({})
-                    break;
-                }
-                case 'user.updated' : {
-                    const userData = {
-                        email : data.email_addresses[0].email_address,
-                        name : data.first_name + " "+ data.last_name,
-                        imageUrl : data.image_url,
-                    }
-                    await User.findByIdAndUpdate(data.id,userData)
-                    res.json({})
-                    break;
-                }
-                case 'user.deleted' : {
-                    await User.findByIdAndDelete(data.id)
-                    res.json({})
-                    break;
-                }
-                default :
-                   break;
-           }
-        }catch(error){
-             res.json({success:false,message : error.message})
-        }
-}
+
+export const clerkWebhooks = async (req, res) => {
+    try {
+      console.log("Request Body:", req.body);
+      
+      const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+      await whook.verify(JSON.stringify(req.body), {
+        "svix-id": req.headers["svix-id"],
+        "svix-timestamp": req.headers["svix-timestamp"],
+        "svix-signature": req.headers["svix-signature"]
+      });
+      
+      const { data, type } = req.body;
+      console.log("Event Type:", type);
+      
+      switch (type) {
+        case 'user.created':
+          const userData = {
+            _id: data.id,
+            email: data.email_addresses[0].email_address,
+            name: data.first_name + " " + data.last_name,
+            imageUrl: data.image_url,
+          };
+          console.log("Creating User:", userData);
+          await User.create(userData);
+          break;
+        
+        case 'user.updated':
+          const updatedUserData = {
+            email: data.email_addresses[0].email_address,
+            name: data.first_name + " " + data.last_name,
+            imageUrl: data.image_url,
+          };
+          console.log("Updating User:", updatedUserData);
+          await User.findByIdAndUpdate(data.id, updatedUserData);
+          break;
+        
+        case 'user.deleted':
+          console.log("Deleting User:", data.id);
+          await User.findByIdAndDelete(data.id);
+          break;
+        
+        default:
+          break;
+      }
+      
+      res.json({});
+    } catch (error) {
+      console.error("Error:", error);
+      res.json({ success: false, message: error.message });
+    }
+  };
+  
+
+
+
+
+
+// export const clerkWebhooks = async(req,res)=>{
+//         try{
+//            const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+//            await whook.verify(JSON.stringify(req.body),{
+//             "svix-id" : req.headers[ "svix-id"],
+//             "svix-timestamp" : req.headers[ "svix-timestamp"],
+//             "svix-signature" : req.headers[ "svix-signature"]
+//            })
+//            const {data,type} = req.body
+//            switch(type){
+//                 case 'user.created' : {
+//                     const userData = {
+//                         _id : data.id,
+//                         email : data.email_addresses[0].email_address,
+//                         name : data.first_name + " "+ data.last_name,
+//                         imageUrl : data.image_url,
+//                     }
+//                     await User.create(userData)
+//                     res.json({})
+//                     break;
+//                 }
+//                 case 'user.updated' : {
+//                     const userData = {
+//                         email : data.email_addresses[0].email_address,
+//                         name : data.first_name + " "+ data.last_name,
+//                         imageUrl : data.image_url,
+//                     }
+//                     await User.findByIdAndUpdate(data.id,userData)
+//                     res.json({})
+//                     break;
+//                 }
+//                 case 'user.deleted' : {
+//                     await User.findByIdAndDelete(data.id)
+//                     res.json({})
+//                     break;
+//                 }
+//                 default :
+//                    break;
+//            }
+//         }catch(error){
+//              res.json({success:false,message : error.message})
+//         }
+// }
